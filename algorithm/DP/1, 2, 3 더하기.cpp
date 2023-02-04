306905fb21b647194d30cb6e58b0a52a1a91e95d
Jiwoo Jeong <90768869+jiwoo0629@users.noreply.github.com>
2023-02-04 22:10:58 +0900

    Update 1, 2, 3 더하기.cpp

--- algorithm/DP/1, 2, 3 더하기.cpp
+++ algorithm/DP/1, 2, 3 더하기.cpp
@@ -7,16 +7,13 @@ int main(void) {
 
     int t;
     cin >> t;
+    int arr[11];
+    arr[1] = 1, arr[2] = 2, arr[3] = 4;
+    for (int i = 4; i <= 11; i++)
+        arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
     while (t--) {
         int n;
         cin >> n;
-        int arr[11];
-        arr[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            if (i == 2) arr[i] = arr[i - 1] + 1;
-            else if (i == 3) arr[i] = arr[i - 1] + arr[i - 2] + 1;
-            else arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
-        }
         cout << arr[n] << "\n";
     }
 
